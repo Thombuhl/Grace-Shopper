62c25e225321cef8a7ba882fa5cbe5c0b7af9c02
WkChris <67604739+enycezchris@users.noreply.github.com>
2022-08-15 01:16:25 -0400

    basic template for homepage

---
+++ src/Products.js
@@ -0,0 +1,222 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Products = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row no-gutters">
+        <div class="col">
+          <div className="card">
+            <div className="card-body">
+              <img
+                class="card-img-top"
+                src="/public/logo.png"
+                alt="Card image cap"
+              />
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="card-link">
+                Add to Cart
+              </a>
+              <a href="#" class="card-link">
+                Wishlist
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className="card">
+            <div className="card-body">
+              <img
+                class="card-img-top"
+                src="/public/logo.png"
+                alt="Card image cap"
+              />
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="card-link">
+                Add to Cart
+              </a>
+              <a href="#" class="card-link">
+                Wishlist
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className="card">
+            <div className="card-body">
+              <img
+                class="card-img-top"
+                src="/public/logo.png"
+                alt="Card image cap"
+              />
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="card-link">
+                Add to Cart
+              </a>
+              <a href="#" class="card-link">
+                Wishlist
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className="card">
+            <div className="card-body">
+              <img
+                class="card-img-top"
+                src="/public/logo.png"
+                alt="Card image cap"
+              />
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="card-link">
+                Add to Cart
+              </a>
+              <a href="#" class="card-link">
+                Wishlist
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className="card">
+            <div className="card-body">
+              <img
+                class="card-img-top"
+                src="/public/logo.png"
+                alt="Card image cap"
+              />
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="card-link">
+                Add to Cart
+              </a>
+              <a href="#" class="card-link">
+                Wishlist
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className="card">
+            <div className="card-body">
+              <img
+                class="card-img-top"
+                src="/public/logo.png"
+                alt="Card image cap"
+              />
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="card-link">
+                Add to Cart
+              </a>
+              <a href="#" class="card-link">
+                Wishlist
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className="card">
+            <div className="card-body">
+              <img
+                class="card-img-top"
+                src="/public/logo.png"
+                alt="Card image cap"
+              />
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="card-link">
+                Add to Cart
+              </a>
+              <a href="#" class="card-link">
+                Wishlist
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className="card">
+            <div className="card-body">
+              <img
+                class="card-img-top"
+                src="/public/logo.png"
+                alt="Card image cap"
+              />
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="card-link">
+                Add to Cart
+              </a>
+              <a href="#" class="card-link">
+                Wishlist
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className="card">
+            <div className="card-body">
+              <img
+                class="card-img-top"
+                src="/public/logo.png"
+                alt="Card image cap"
+              />
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="card-link">
+                Add to Cart
+              </a>
+              <a href="#" class="card-link">
+                Wishlist
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className="card">
+            <div className="card-body">
+              <img
+                class="card-img-top"
+                src="/public/logo.png"
+                alt="Card image cap"
+              />
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="card-link">
+                Add to Cart
+              </a>
+              <a href="#" class="card-link">
+                Wishlist
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Products;
