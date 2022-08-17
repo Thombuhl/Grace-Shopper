401b0afe9e1fc0aae3d45dfa212dd57624931302
WkChris <67604739+enycezchris@users.noreply.github.com>
2022-08-17 13:51:45 -0400

    add component style sheet

--- src/Carousel.js
+++ src/Carousel.js
@@ -3,75 +3,18 @@ import styled from "styled-components";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { carouselData } from "./carouselData";
-
-const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-  display: flex;
-  position: relative;
-  overflow: hidden;
-`;
-
-const ArrowDirection = styled.div`
-  width: 50px;
-  height: 50px;
-  background-color: lightgray;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  margin: auto;
-  left: ${(props) => props.direction === "back" && "10px"};
-  right: ${(props) => props.direction === "forward" && "10px"};
-  cursor: pointer;
-  opacity: 0.35;
-  z-index: 3;
-`;
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  transition: all 1.5s ease;
-  transform: translatex(${(props) => props.slideItem * -100}vw);
-`;
-
-const Slide = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-`;
-const ImageDiv = styled.div`
-  height: 100%;
-  flex: 1;
-`;
-
-const Image = styled.img`
-  height: 100%;
-  width: 60vw;
-`;
-
-const InfoDiv = styled.div`
-  flex: 1;
-`;
-const Title = styled.h1`
-  font-size: 70px;
-`;
-const Info = styled.p`
-  margin: 50px 0;
-  font-size: 20px;
-  font-weight: 500;
-  letter-spacing: 2px;
-`;
-const Button = styled.button`
-  padding: 10px;
-  font-size: 20px;
-  background-color: transparent;
-  cursor: pointer;
-`;
+import {
+  Container,
+  ArrowDirection,
+  Wrapper,
+  Slide,
+  Info,
+  Image,
+  ImageDiv,
+  InfoDiv,
+  Title,
+  Button,
+} from "./styledComponents/CarouselStyles";
 
 const Carousel = () => {
   const [slideItem, setSlideItem] = useState(0);
