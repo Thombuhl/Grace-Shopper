757fabf3ee346c1cd1f669d159ae08aa3d03077a
WkChris <67604739+enycezchris@users.noreply.github.com>
2022-08-18 01:00:50 -0400

    getting and displaying products data on products page

--- src/Cart.js
+++ src/Cart.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { deleteLineItem, exchangeToken, fetchCart } from "./store";
-import {updateLineItemQuantity} from './store';
-import { Link } from "react-router-dom";
-
+import { deleteLineItem, exchangeToken, fetchCart } from './store';
+import { updateLineItemQuantity } from './store';
+import { Link } from 'react-router-dom';
 
 class Cart extends Component {
   constructor(props) {
@@ -12,68 +11,73 @@ class Cart extends Component {
     this.onChange = this.onChange.bind(this);
   }
 
-
   componentDidUpdate(prevProps) {
-    if(!prevProps.auth.id && this.props.auth.id) {
+    if (!prevProps.auth.id && this.props.auth.id) {
       this.props.fetchCart();
-    };
-  };
+    }
+  }
 
   onChange(ev) {
-    const change ={[ev.target.name]: ev.target.value};
+    const change = { [ev.target.name]: ev.target.value };
     this.setState(change);
     this.props.updateLineItemQuantity(change);
   }
 
   render() {
-
-    const {cart} = this.props;
-    const {onChange} = this;
+    const { cart } = this.props;
+    const { onChange } = this;
 
     return (
       <main>
         <h1>My Cart</h1>
-          <ul>
-          {
-            cart.lineItems.map( lineItem => {
-              return (
-                <li key={ lineItem.id }>
-                  { lineItem.product.name } { lineItem.quantity } 
-                  <input type='number' name={lineItem.product.name} value={lineItem.quantity} onChange={onChange}/>
-                  <p> Size:{lineItem.product.size}</p>
-                  <p>Color:{lineItem.product.colorway}</p>
-                  <p>Price:{lineItem.product.Price}</p>
-                  <p>About:{lineItem.product.description}</p>
-                  <button onClick={() => this.props.deleteLineItem()}>X</button>
-                </li>
-                  )
-            })
-          }
-          </ul>
-          <section id="cost">
-            <p>Shipping Cost</p>
-            <p>Discount</p>
-            <p></p>
-          </section>
-          <button><Link className="links" to='/checkout'>Checkout</Link></button> 
-    </main>
+        <ul>
+          {cart.lineItems.map((lineItem) => {
+            return (
+              <li key={lineItem.id}>
+                {lineItem.product.name} {lineItem.quantity}
+                <input
+                  type="number"
+                  name={lineItem.product.name}
+                  value={lineItem.quantity}
+                  onChange={onChange}
+                />
+                <p> Size:{lineItem.product.size}</p>
+                <p>Color:{lineItem.product.colorway}</p>
+                <p>Price:{lineItem.product.Price}</p>
+                <p>About:{lineItem.product.description}</p>
+                <button onClick={() => this.props.deleteLineItem()}>X</button>
+              </li>
+            );
+          })}
+        </ul>
+        <section id="cost">
+          <p>Shipping Cost</p>
+          <p>Discount</p>
+          <p></p>
+        </section>
+        <button>
+          <Link className="links" to="/checkout">
+            Checkout
+          </Link>
+        </button>
+      </main>
     );
   }
 }
 
-const mapStateToProps = ({cart, auth}) => {
+const mapStateToProps = ({ cart, auth }) => {
   return {
     cart,
-    auth
+    auth,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-  exchangeToken:  () => dispatch(exchangeToken()),
-  fetchCart:  () => dispatch(fetchCart()),
-  updateLineItemQuantity: (obj) => dispatch(updateLineItemQuantity(obj)),
-  deleteLineItem: (item) => dispatch(deleteLineItem(item))
+    exchangeToken: () => dispatch(exchangeToken()),
+    fetchCart: () => dispatch(fetchCart()),
+    updateLineItemQuantity: (obj) => dispatch(updateLineItemQuantity(obj)),
+    deleteLineItem: (item) => dispatch(deleteLineItem(item)),
   };
 };
 
