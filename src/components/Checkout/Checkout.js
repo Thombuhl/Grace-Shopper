758d601a0b18b21902bdc262ff138aed546f8001
doobybooby <0933300+doobybooby@users.noreply.github.com>
2022-08-23 09:16:09 -0400

    allow webpack to load .css, create a separate css for checkout, set up basic checkout page

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,38 +1,43 @@
-import axios from 'axios'
 import React from 'react'
 import { useSelector } from 'react-redux'
-import './checkout.css'
+import '../../../public/checkout.css'
 
 const Checkout = () => {
   const userCart = useSelector(state => state.cart)
   console.log(userCart)
 
-  const checkoutCart = async(e) => {
-    e.preventDefault()
-    // logic to checkout a cart
-  }
+  const total = userCart.lineItems.reduce((accum, item)=>{
+    return accum += (item.product.price * item.quantity)
+  }, 0)
 
   return (
-    <div>
-      CHECKOUT 
-      <ol>
-        {
-          userCart.lineItems.map( item => {
-            return (
-              <li key={item.id}>
-                <h3>{ item.product.name }</h3>
-                <h4>{ item.product.colorway }</h4>
-                <h5>QTY: { item.quantity }</h5>
-                <h5>QTY: { item.product.price }</h5>
-                <img src={item.product.imageLocation} alt='item' width='20%'/>
-              </li>
-            )
-          })
-        }
-      </ol>
-      <form onSubmit={checkoutCart}>
-        <input type="text" placeholder='DISCOUNT CODE' />
-        <button> CHECKOUT</button>
+    <div className='component'>
+      <a href="">LOGIN and CHECKOUT FASTER</a>
+      <form action="">
+        <h3>Contact Details</h3>
+        <p>We'll use these details to keep you informed on your delivery.</p>
+        <input type="text" placeholder='Email *' className='checkout-input'/>
+        <h3>Contact Details</h3>
+        <div>
+          <input type="text" placeholder='First Name *' />
+          <input type="text" placeholder='Last Name *' />
+        </div>
+        <input type="text" placeholder='Street Address, PO Box *' />
+        <input type="text" placeholder='Apartment/Unit (optional)' />
+        <div>
+          <input type="text" placeholder='City/Town *' />
+          <input type="text" placeholder='State *' />
+        </div>
+        <input type="text" placeholder='Zip Code *' />
+        <h4>Country: USA</h4>
+        <h2>Delivery Option</h2>
+        <input type="radio" name='shipping' /><label htmlFor="">Standard</label>
+        <input type="radio" name='shipping' /><label htmlFor="">Express</label>     
+        <input type="checkbox" /> <label htmlFor="">My billing and delivery information are the same</label>
+        <input type="checkbox" /> <label htmlFor="">I'm 13+ years old *</label>
+        <h5>Also want product updates with our newsletter?</h5>
+        <input type="checkbox" /> <label htmlFor="">Yes, I'd like to receive emails about exclusive sales and more</label>
+        <button>Review and Pay</button>
       </form>
     </div>
   )
