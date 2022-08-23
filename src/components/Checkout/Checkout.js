079c9a2eb50ce191196e01f643eab4a46eeceaf0
doobybooby <0933300+doobybooby@users.noreply.github.com>
2022-08-23 10:30:41 -0400

    checkout page is decently ready, need to link REVIEW&PAY and apply css

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -4,41 +4,85 @@ import '../../../public/checkout.css'
 
 const Checkout = () => {
   const userCart = useSelector(state => state.cart)
-  console.log(userCart)
-
-  const total = userCart.lineItems.reduce((accum, item)=>{
+  const cartTotal = userCart.lineItems.reduce((accum, item)=>{
     return accum += (item.product.price * item.quantity)
+  }, 0).toFixed(2)
+  const totalItems = userCart.lineItems.reduce((accum, item)=>{
+    return accum += item.quantity 
   }, 0)
-
+  const delivery = (cartTotal > 150 ? 0 : .05 * cartTotal).toFixed(2)
+  const tax = (.04 * cartTotal).toFixed(2)
+  const finalPrice = (cartTotal*1 + delivery*1 + tax*1).toFixed(2) 
   return (
-    <div className='component'>
-      <a href="">LOGIN and CHECKOUT FASTER</a>
+    <div className='checkout-component'>
       <form action="">
-        <h3>Contact Details</h3>
-        <p>We'll use these details to keep you informed on your delivery.</p>
-        <input type="text" placeholder='Email *' className='checkout-input'/>
-        <h3>Contact Details</h3>
+        {/* <a href="">LOGIN & CHECKOUT FASTER</a> */}
+        <div>
+          <h3>Contact Details</h3>
+          <p>We'll use these details to keep you informed on your delivery.</p>
+          <input type="text" placeholder='Email *'/>
+        </div>
+        <h3>Shipping Address</h3>
         <div>
           <input type="text" placeholder='First Name *' />
           <input type="text" placeholder='Last Name *' />
         </div>
-        <input type="text" placeholder='Street Address, PO Box *' />
-        <input type="text" placeholder='Apartment/Unit (optional)' />
+        <div>
+          <input type="text" placeholder='Street Address, PO Box *'/>
+        </div>
+        <div>
+          <input type="text" placeholder='Apartment/Unit (optional)'  />
+        </div>
         <div>
           <input type="text" placeholder='City/Town *' />
           <input type="text" placeholder='State *' />
         </div>
-        <input type="text" placeholder='Zip Code *' />
-        <h4>Country: USA</h4>
-        <h2>Delivery Option</h2>
-        <input type="radio" name='shipping' /><label htmlFor="">Standard</label>
-        <input type="radio" name='shipping' /><label htmlFor="">Express</label>     
-        <input type="checkbox" /> <label htmlFor="">My billing and delivery information are the same</label>
-        <input type="checkbox" /> <label htmlFor="">I'm 13+ years old *</label>
+        <div>
+          <input type="text" placeholder='Zip Code *' />
+        </div>
+        <h6>Country: USA</h6>
+        <h2>Delivery Option [?]</h2>
+        <label htmlFor="">Standard  <input type="radio" name='shipping' className='shipping'/><p>3-5 bussiness day</p></label>
+        <label htmlFor="">Express <input type="radio" name='shipping' className='shipping'/><p>24 hour</p></label>
+        <label htmlFor="">Pickup  <input type="radio" name='shipping' className='shipping'/><p>Pick it up in our store</p></label>
+        <label htmlFor=""><input type="checkbox" /> My billing and delivery information are the same</label>
+        <label htmlFor=""><input type="checkbox" /> I'm 13+ years old *</label>
         <h5>Also want product updates with our newsletter?</h5>
-        <input type="checkbox" /> <label htmlFor="">Yes, I'd like to receive emails about exclusive sales and more</label>
+        <label htmlFor=""> <input type="checkbox" /> Yes, I'd like to receive emails about exclusive sales and more</label>
         <button>Review and Pay</button>
       </form>
+        <div>
+          <h5>Order Summary</h5>
+          <p>{ totalItems } Items ${ cartTotal }</p>
+          <p>DELIVERY: ${ delivery }
+          </p>
+          <p>Sales Tax: ${ tax }</p>
+          <h5>TOTAL ${ finalPrice }</h5>
+          <input type="text" placeholder='Enter your promo code'/>
+          <button>+</button>
+          <div>
+            <h5>Order Details</h5>
+            {
+              userCart.lineItems.map( item => (
+                <li key={item.id} className='flex-row'>
+                  <img src={item.product.imageLocation} alt="product" width='30%' />
+                  <div>
+                    <p>{item.product.name}</p>
+                    <p>{item.product.colorway}</p>
+                    <p>SIZE: {item.product.size} / QTY: {item.quantity}</p>
+                    <p>${item.product.price}</p>
+                  </div>
+                </li>
+              ))
+            }
+          </div>
+          <h6>ACCEPTED PAYMENT METHODS</h6>
+          <div>
+            <img src="" alt="MasterCard Icon" />
+            <img src="" alt="AMEX Icon" />
+            <img src="" alt="Visa Icon" />
+          </div>
+        </div>
     </div>
   )
 }
