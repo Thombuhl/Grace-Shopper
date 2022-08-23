f79fade920d94afc5f782576be0ff0e0bcf806b0
Thombuhl <thomas.m.buhl@gmail.com>
2022-08-22 20:12:01 -0400

    added stripe components

--- src/stripe/CheckoutForm.js
+++ src/stripe/CheckoutForm.js
@@ -1,25 +1,64 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import {CardElement, useElements, useStripe} from "@stripe/stripe-js";
+import {CardElement, useElements, useStripe} from "@stripe/react-stripe-js"
+
+const CARD_OPTIONS = {
+
+}
 
 
-const Payment = () => {
-  const [sucess, setSuccess] = useState(false)
+const PaymentForm = () => {
+  const [success, setSuccess] = useState(false)
   const stripe = useStripe();
   const elements = useElements();
 
 
-  const handlePayment = async(evt) => {
+  const handleSubmit = async(evt) => {
     evt.preventDefault();
-    const [error, paymentMethod] = await stripe.createPaymentMethod({
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
       type: "card",
       card: elements.getElement(CardElement)
     })
-  }
+
+    if(!error) {
+      try {
+        const {id} = paymentMethod
+        const response = await axios.post('/api/orders/checkout', {
+          amount: 1000, 
+          id
+        })
+          
+        if(response.data.success) {
+          console.log("confirmed")
+          setSuccess(true)
+      }
+      } catch (error) {
+        console.log('There was an error', error)
+      }
+
+    } else {
+      console.log(error)
+    }
+}
   return (
-    <div></div>
+    <>
+    {!success ? 
+        <form onSubmit={handleSubmit}>
+          <fieldset className='FormGroup'></fieldset>
+          <div className="FormRow">
+            <CardElement options={CARD_OPTIONS}/>
+          </div>
+          <fieldset>
+            <button>Pay</button>
+          </fieldset>
+        </form>
+        : <div>
+          <h2>You Bought something</h2>
+        </div>
+    }
+    </>
   )
 }
 
 
-export default Payment
+export default PaymentForm
