2fed74b3b7b733d61a6417868d680e7125280853
doobybooby <0933300+doobybooby@users.noreply.github.com>
2022-08-27 10:31:55 -0400

    adding files

---
+++ src/components/Error/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../../public/errorPage.css'
+
+export const ErrorPage = () => {
+  return (
+    <div className='error-component'>
+      <h1>COMING SOON!</h1>
+      <img src="../../public/sven.png" alt="" />
+    </div>
+  )
+}
